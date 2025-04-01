d32e7c35f71aced1a1528992e7cc9fdd73d9658e
Raj Miyani <166621455+rajmiyani@users.noreply.github.com>
2025-04-01 21:06:35 +0530

    Add files via upload

---
+++ SplineScene .js
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+const SplineScene = () => {
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <Spline scene="https://prod.spline.design/7ix3k8IoIhm8AdvH/scene.splinecode" />
+    </div>
+  );
+};
+
+export default SplineScene;
